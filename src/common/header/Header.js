7b89e7d5a80bf1e10356ec25ad745e5e48956cb1
AuraGrande <97280360+AuraGrande@users.noreply.github.com>
2022-08-28 20:02:53 +0530

    Code Refactoring, added login/register modal
    Refactered the code to use hooks instead of classes. Cleared two warnings that was thrown by the browser. Added a animated Login and Register modal. Todo: Need to add the tabs and text fields within the Modal.

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, useState } from "react";
 import Button from "@material-ui/core/Button";
 import './Header.css';
+import LoginRegister from '../LoginRegister/LoginRegister.js';
 
-function Header(){
-    var mainHeader = (<div id='headprops'>
-    <table class='tableprops'>
-        <tr class='tableprops'>
-            <th class='rowprops2'>
-                <div id='logoattribs'></div>
-            </th>
-            <th class='rowprops1'>
-            </th>
-            <th class='rowprops2'>
-                <Button variant="contained" color='primary'>Book Show</Button>
-            </th>
-            <th class='rowprops2'>
-                <Button variant="contained" color='default'>Login</Button>
-            </th>
-        </tr>
-    </table>
-</div>);
+function Header() {
+    const [isDetailsPage, setBookShowBtnState] = useState(false);
+
+    const bookShowbtn = (<Button variant="contained" color='primary'>Book Show</Button>);
+    const noshowbtn = (<p></p>);
+
+    const mainHeader = (<div id='headprops'>
+            <table className='tableprops'>
+                <tbody>
+                    <tr className='tableprops'>
+                        <th className='rowprops2'>
+                            <div id='logoattribs'></div>
+                        </th>
+                        <th className='rowprops1'>
+                        </th>
+                        <th className='rowprops2'>
+                            { isDetailsPage?bookShowbtn:noshowbtn }
+                        </th>
+                        <th className='rowprops2'>
+                            <LoginRegister />
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+
     return mainHeader;
 }
 
